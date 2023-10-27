2dddec4b94b6071f2b2e15da3b761bc9b81953a7
archita-b <archita.bhattacharyya91@gmail.com>
2023-10-27 21:37:39 +0530

    google oauth using nextauth

--- app/page.js
+++ app/page.js
@@ -1,27 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import GoogleButton from "react-google-button";
+import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
   return (
-    <div className="flex justify-center items-center h-screen bg-sky-100">
-      <div className="px-7 py-4 shadow bg-white rounded-md flex flex-col gap-2 space-y-2">
-        <label className="flex flex-col">
-          Username
-          <input className="shadow rounded border-[2px] border-blue-200"></input>
-        </label>
-        <label className="flex flex-col">
-          Password
-          <input className="shadow rounded border-[2px] border-blue-200"></input>
-        </label>
-        <div className="flex justify-center">
-          <button className="shadow rounded p-2 bg-blue-500 text-white font-medium">
-            Login / SignUp
-          </button>
-        </div>
-      </div>
-    </div>
+    <main className="flex justify-center items-center h-screen">
+      <GoogleButton onClick={() => signIn("google")} />
+    </main>
   );
 }
