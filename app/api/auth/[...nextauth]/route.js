157d1e2b37a3488abc4ca74baf8e19112eba4c86
archita-b <archita.bhattacharyya91@gmail.com>
2023-10-28 17:15:21 +0530

    credentials provider in nextauth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,35 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          type: "text",
+          placeholder: "Username",
+        },
+        password: {
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials, req) {
+        const user = { id: "1", name: "archita", password: "123" };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        }
+        return null;
+      },
     }),
   ],
   secret: process.env.SECRET,
