7d8af7c7b90e55bcd32306152ce67fd44fc78faf
archita-b <archita.bhattacharyya91@gmail.com>
2023-10-31 12:49:18 +0530

    configure lessons page for authorized users

--- app/lessons/page.js
+++ app/lessons/page.js
@@ -3,10 +3,24 @@ import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
 export default async function LessonsPage() {
+  const userData = [
+    "archita.bhattacharyya91@gmail.com",
+    "santosh@geekskool.com",
+  ];
+
   const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=/lessons");
   }
-  return <div>This is the Lessons page</div>;
+  const authorizedUser = userData.includes(session.user.email);
+  return (
+    <>
+      {authorizedUser ? (
+        <div>Welcome {session.user.name} </div>
+      ) : (
+        <div>You are not allowed</div>
+      )}
+    </>
+  );
 }
