6f79d71a18203582e1a145c983f84617ed1cc4dd
archita-b <archita.bhattacharyya91@gmail.com>
2023-11-03 12:35:50 +0530

    add css in lessons page

--- app/lessons/page.js
+++ app/lessons/page.js
@@ -23,11 +23,25 @@ export default async function LessonsPage() {
         <div>You are not allowed </div>
       ) : (
         <>
-          <div>Welcome {session.user.name}</div>
-          <div>
-            {lessons.map((lesson) => {
-              return <div key={lesson.id}>{lesson.id}</div>;
-            })}
+          <div className="flex justify-center">
+            <h1 className="text-2xl">Welcome {session.user.name}</h1>{" "}
+          </div>
+          <div className="flex flex-col justify-center items-center h-screen">
+            <h1 className="mb-10 font-bold text-3xl text-slate-600">
+              Choose a Lesson
+            </h1>
+            <div className="w-96 h-96 p-4 grid grid-cols-2 gap-6">
+              {lessons.map((lesson) => {
+                return (
+                  <button
+                    key={lesson.id}
+                    className={`relative rounded-xl border-x-2 border-t-2 border-b-4 border-slate-300 text-slate-500 font-semibold text-3xl hover:bg-slate-200`}
+                  >
+                    Lesson {lesson.id}
+                  </button>
+                );
+              })}
+            </div>
           </div>
         </>
       )}
