502fab9bf177c7cec84240e4b9fbaae6f3270792
archita-b <archita.bhattacharyya91@gmail.com>
2023-11-08 16:44:27 +0530

    update lessons/route.js

--- app/api/lessons/route.js
+++ app/api/lessons/route.js
@@ -1,7 +1,7 @@
 import pool from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
+export async function GET() {
   const result = await pool.query("SELECT * FROM lessons");
   const lessonsData = result.rows;
   return NextResponse.json(lessonsData);
