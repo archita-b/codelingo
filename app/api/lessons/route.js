6e23ecb13e03734696ae16b835e7a2602cfd7028
archita-b <archita.bhattacharyya91@gmail.com>
2023-11-08 17:25:13 +0530

    modify error msg in lessons/route.js

--- app/api/lessons/route.js
+++ app/api/lessons/route.js
@@ -1,8 +1,12 @@
-// import pool from "@/lib/db";
+import pool from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  // const result = await pool.query("SELECT * FROM lessons");
-  // const lessonsData = result.rows;
-  return NextResponse.json();
+export async function GET(req) {
+  const result = await pool.query("SELECT * FROM lessons");
+  const lessonsData = result.rows;
+
+  if (!lessonsData)
+    return NextResponse.json({ error: "Lessons not found" }, { status: 404 });
+
+  return NextResponse.json(lessonsData);
 }
