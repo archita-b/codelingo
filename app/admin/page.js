1c649aa5128bfcdb8d766497bad2330360755b2a
archita-b <archita.bhattacharyya91@gmail.com>
2023-11-28 22:14:14 +0530

    create question on admin page

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,26 +1,62 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { createQuestion, getLessons } from "../components/requests";
 
 export default function AdminPage() {
-  const [questionType, setQuestionType] = useState([]);
-  const [lessonId, setLessonId] = useState([]);
+  const [lessonsData, setLessonsData] = useState([]);
+  const [lessonId, setLessonId] = useState(0);
+  const [questionType, setQuestionType] = useState("");
   const [question, setQuestion] = useState("");
-  const [answers, setAnswers] = useState([]);
-  const [correctAnswer, setCorrectAnswer] = useState("");
+  const [answers, setAnswers] = useState(["", "", "", ""]);
+  const [correctAnswer, setCorrectAnswer] = useState(0);
+
+  useEffect(() => {
+    getLessons().then((data) => {
+      if (data) {
+        const lessonsArray = data.data;
+        setLessonsData(lessonsArray);
+      }
+    });
+  }, []);
+
+  // ["Answer 1", "Answer 2", "Answer 3", "Answer 4"]
+
+  function addQuestion(e) {
+    e.preventDefault();
+    createQuestion(
+      questionType,
+      lessonId,
+      question,
+      answers,
+      correctAnswer
+    ).then((data) => data);
+  }
+
+  function setAnswerOptions(index, value) {
+    setAnswers((answer) => {
+      answer[index] = value;
+      return [...answer];
+    });
+  }
 
   return (
-    <form>
-      This is admin page
+    <form onSubmit={addQuestion}>
+      <p>This is admin page</p>
       <h1>Create a question:</h1>
       <div className="flex flex-col">
         <label>
-          select a type
-          <select>
-            {questionType.map((type) => {
+          Select chapter
+          <select
+            value={lessonId}
+            onChange={(e) => {
+              setLessonId(e.target.value);
+            }}
+          >
+            {lessonsData.map((lessonData) => {
               return (
-                <option key={type.question_type} value={type.question_type}>
-                  {type.question_type}
+                <option key={lessonData.lesson_name} value={lessonData.id}>
+                  {lessonData.lesson_name}
                 </option>
               );
             })}
@@ -28,11 +64,15 @@ export default function AdminPage() {
         </label>
 
         <label>
-          Enter lesson id
-          <select>
-            {lessonId.map((id) => {
-              return <option key={id.id}>{id.id}</option>;
-            })}
+          select question type
+          <select
+            value={questionType}
+            onChange={(e) => {
+              setQuestionType(e.target.value);
+            }}
+          >
+            <option>none</option>
+            <option value="mcq">mcq</option>
           </select>
         </label>
 
@@ -46,27 +86,40 @@ export default function AdminPage() {
         </label>
 
         <label>
-          Enter possible answers in an array
-          <textarea
-            placeholder="type answers"
-            value={answers}
-            onChange={(e) => setAnswers(e.target.value)}
-          />
+          Enter possible answers:
+          {answers.map((element, index) => {
+            return (
+              <input
+                key={index}
+                placeholder={`Type answer ${index + 1}`}
+                value={element}
+                onChange={(e) => setAnswerOptions(index, e.target.value)}
+              />
+            );
+          })}
         </label>
 
         <label>
           Correct answer
-          <select>
-            {Array.from(
-              { length: answers.length },
-              (value, index) => index + 1
-            ).map((option) => {
-              return <option key={option}>{option}</option>;
-            })}
+          <select
+            value={correctAnswer}
+            onChange={(e) => {
+              setCorrectAnswer(e.target.value);
+            }}
+          >
+            {Array.from({ length: 4 }, (value, index) => index + 1).map(
+              (element) => {
+                return (
+                  <option key={element} value={element}>
+                    {element}
+                  </option>
+                );
+              }
+            )}
           </select>
         </label>
       </div>
-      <button type="submit">submit question</button>
+      <button>submit question</button>
     </form>
   );
 }
--- app/api/admin/route.js
+++ app/api/admin/route.js
@@ -7,10 +7,10 @@ export async function POST(req) {
   const question = await prisma.question.create({
     data: {
       question_type: body.question_type,
-      lesson_id: body.lesson_id,
+      lesson_id: parseInt(body.lesson_id),
       question: body.question,
       answers: body.answers,
-      correct_ans: body.correct_ans,
+      correct_ans: parseInt(body.correct_ans),
     },
   });
 
