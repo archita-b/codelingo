41fc544949c96179c566866fe4535c3bd3acf74c
archita-b <archita.bhattacharyya91@gmail.com>
2023-12-14 15:22:02 +0530

    update Code Mirror

--- app/components/JSEditor.jsx
+++ app/components/JSEditor.jsx
@@ -1,18 +1,16 @@
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
+// import { okaidia } from "@uiw/codemirror-theme-okaidia";
 import { useCallback, useState } from "react";
 
-export default function JSEditor() {
-  const [value, setValue] = useState("console.log('hello world!');");
-  const onChange = useCallback((val, viewUpdate) => {
-    console.log("val:", val);
-    setValue(val);
-  }, []);
+export default function JSEditor({ value, onChange }) {
   return (
     <CodeMirror
+      className="w-full h-60 border border-gray-400 rounded-md"
       value={value}
       height="200px"
       extensions={[javascript({ jsx: true })]}
+      //   theme={okaidia}
       onChange={onChange}
     />
   );
--- app/components/OtherQuestionType.jsx
+++ app/components/OtherQuestionType.jsx
@@ -1,19 +1,32 @@
+import { useCallback, useState } from "react";
 import JSEditor from "./JSEditor";
 
 export default function OtherQuestionType({ currentQuestion }) {
+  const [value, setValue] = useState("console.log('hi')");
+
+  const onChange = useCallback((val) => {
+    setValue(val);
+  }, []);
+
+  function evaluateCode(code) {
+    try {
+      const func = new Function(code);
+      return func();
+    } catch (err) {
+      console.error("Error: ", err.message);
+    }
+  }
+
   return (
-    <div>
+    <div className="flex flex-col gap-3">
       <h1>{currentQuestion.question}</h1>
       <div>
-        <JSEditor />
+        <JSEditor value={value} onChange={onChange} />
       </div>
       <div>
-        <iframe
-          title="output"
-          sandbox="allow-scripts"
-          frameBorder="0"
-          width="100%"
-          height="100%"
+        <textarea
+          className="w-full h-20 border border-gray-400 rounded-md p-3"
+          value={evaluateCode(value)}
         />
       </div>
     </div>
